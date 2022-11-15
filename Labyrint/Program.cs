bbdd33d42e2cef1532ebe4ac37741e3df1a53188
Frejbo <frej.hellsten@gmail.com>
2022-11-15 11:09:22 +0100

    first

---
+++ Labyrint/Program.cs
@@ -0,0 +1,17 @@
+﻿using Raylib_cs;
+using Noise();
+
+// hej = new Libnoise.
+// noiseMap = new 
+
+Raylib.InitWindow(500, 500, "Labyrint");
+
+while (!Raylib.WindowShouldClose()) {
+    draw_game();
+}
+
+void draw_game() {
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.BLACK);
+    Raylib.EndDrawing();
+}
